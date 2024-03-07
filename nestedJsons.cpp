4c98669d1eb0fcca364956afd4650d06d487e786
jay1499 <36840105+jay1499@users.noreply.github.com>
2024-03-07 17:50:27 +0530

    Update nestedJsons.cpp

--- nestedJsons.cpp
+++ nestedJsons.cpp
@@ -1,77 +1,87 @@
-// Online C++ compiler to run C++ program online
 #include <bits/stdc++.h>
 using namespace std;
 
-void parse(string s, map<string, vector<string>>&m) {
-    
-    if(s[0] == '{') {
-        int i = 1;
-        
-        while(i<s.size()) {
-            string key = s.substr(i, s.find(':', i) - i);
-            string val = s.substr(s.find(':', i) +1,  s.find(',', i));
-            
-            if(val[0] == '{') {
-                map<string, vector<string>> inner;
-                parse(val, inner);
-                
-                //recursion for inner nested objects
-                m[key] = inner;
-            } else {
-                if(m.find(key) != m.end())
-                    m.push_back(val);
-                else {
-                    vector<string>temp;
-                    temp.push_back(key);
-                    m[key] = val;
-                }
-            }
-            i = s.find(',', i) + 1;
+
+void parse(const string& s, map<string, vector<string>>& m) {
+    int i = 0;
+    while (i < s.size()) {
+        string key = s.substr(i, s.find(':', i) - i);
+        string val;
+        int next = s.find(',', i);
+        if (next != string::npos) {
+            val = s.substr(s.find(':', i) + 1, next - (s.find(':', i) + 1));
+            i = next + 1;
+        } else {
+            val = s.substr(s.find(':', i) + 1);
+            i = s.size();
         }
-    } 
+
+        if (val[0] == '{') {
+            map<string, vector<string>> inner;
+            parse(val, inner);
+            // recursion for inner nested objects
+            m[key] = inner;
+        } else {
+            vector<string> temp = split(val, ',');
+            m[key] = temp;
+        }
+    }
 }
 
-void printVal(map<string, vector<string>>m, string key) {
+string printVal(const map<string, vector<string>>& m, const string& key) {
     int pos = key.find(".");
 
-    //non-nested cases
+    // non-nested cases
     if (pos == string::npos) {
-        return m[key];
+        if (m.find(key) != m.end()) {
+            string result;
+            for (const auto& value : m.at(key)) {
+                result += value + " ";
+            }
+            return result;
+        } else {
+            return "Null";
+        }
     }
-    
-    vector<string> innerJsons = split(key, ".");
+
+    vector<string> innerJsons = split(key, '.');
     map<string, vector<string>> innerMap = m;
-    
-    for(auto x:innerJsons) {
-        if(innerMap.count(x) == 0)
-            return;
+
+    for (const auto& x : innerJsons) {
+        if (innerMap.count(x) == 0) {
+            return "Null";
+        }
         innerMap = innerMap[x];
     }
-    
-    string ans  = "";
-    for(auto x: innerMap[innerJsons.back()]) {
-        ans+= x;
+
+    string result;
+    for (const auto& value : innerMap[innerJsons.back()]) {
+        result += value + " ";
     }
-    return ans;
-    
+    return result;
 }
+
+vector<string> split(const string& s, char d) {
+    vector<string> tokens;
+    string token;
+    istringstream tokenStream(s);
+    while (getline(tokenStream, token, d)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
 int main() {
-    // Write C++ code here
     string s;
-    int n,q;
-    cin>>s;
-    cin>>n>>q;
-    map<string, vector<string>>m;
+    int n, q;
+    cin >> s >> n >> q;
+    map<string, vector<string>> m;
     parse(s, m);
-    
-    //sample string 
-    // string json ="{\firstName\": \"Steve\", 
-    // "\address\": {\"street\" : \"1st cross\"}}";
-    
-    for(int i= 0; i<q;i++) {
+
+    for (int i = 0; i < q; i++) {
         string key;
-        cin>>key;
-        cout<<printValue(m, key)<<endl;
+        cin >> key;
+        cout << printVal(m, key) << endl;
     }
 
     return 0;
